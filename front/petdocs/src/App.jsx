e2803fd7e64f3e5a904905a6619cf5c3a96c8aa1
leonAsturizaga <leonasturizaga@gmail.com>
2024-05-28 22:09:22 -0300

    feature/hero add 202405282209

---
+++ front/petdocs/src/App.jsx
@@ -0,0 +1,17 @@
+//import { useState } from 'react'
+import Hero from './components/Hero'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Hero />
+      </div>
+      
+    </>
+  )
+}
+
+export default App
+
