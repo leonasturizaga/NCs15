146be7aa80cd400c686d8c8edddf6ff27f5b7867
OsvaldoRamirez97 <osvaldo97@live.com.ar>
2024-05-31 17:32:39 -0300

    Agregando Login y arreglando Register

--- petdocs/src/pages/Register.jsx
+++ petdocs/src/pages/Register.jsx
@@ -24,23 +24,34 @@ const Register = () => {
     e.preventDefault(); 
 
     let regxp = /^([a-zA-Z0-9-]){1,16}$/;
+    const errors = [];
 
+    //Verificación de contraseña
     if (password !== repeatPassword) {
-      alert('Las contraseñas no coinciden');
-    }
-
-    if(password.length < 8) {
-      alert('Contraseña débil');
-    }else if(password.length > 12) {
-      alert('Contraseña muy larga')
-    }else {
-      if(regxp.test(password) === false){
-        alert('Contraseña no valida')
-      } else {
-        alert('Contraseña exitosa')
-        return
+      errors.push('Las contraseñas no coinciden');
+    } else {
+      if(password.length < 8) {
+        errors.push('Contraseña débil');
+      } else if(password.length > 12) {
+        errors.push('Contraseña muy larga');
+      } else if (!regxp.test(password)) {
+        errors.push('Contraseña no válida');
       }
     }
+
+    //Verificación de usuario
+    if(nick.length < 3) {
+      errors.push('El usuario debe contener un mínimo de 3 caracteres');
+    } else if(nick.length > 20) {
+      errors.push('El usuario puede contener un máximo de 20 caracteres');
+    } else if (!regxp.test(nick)) {
+      errors.push('Usuario no válido');
+    }
+
+    if (errors.length > 0) {
+      errors.forEach(error => alert(error));
+      return;
+    } 
 
     const formData = {
       nick : nick,
@@ -51,7 +62,7 @@ const Register = () => {
 
     const sendLoginRequest = async () => {
       try {
-        const response = await fetch('', {
+        const response = await fetch('https://ncs15-petdocs-api.onrender.com/create_owner/', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
