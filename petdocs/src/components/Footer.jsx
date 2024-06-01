50d250737d60762b0ee3c7c2d912fa8fda8c3060
Frere-Luz <algo_de_luz@hotmail.com>
2024-05-31 21:06:34 -0300

    Secciones Banner y Footer completas

--- petdocs/src/components/Footer.jsx
+++ petdocs/src/components/Footer.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import './css/Footer.css';
+import ImagenLogo from "../assets/LogoPetDocs.png";
+import ImagenInstagram from "../assets/iconoInstagram-alfa.png";
+import ImagenWhatsapp from "../assets/iconoWhatsapp-alfa.png";
+import ImagenMail from "../assets/iconoMail-alfa.png";
 
-const Footer = () => (
-  <footer>
-    <p>&copy; 2024 My Website Footer</p>
-  </footer>
-);
 
-export default Footer;
+const Footer = () => {
+    return (
+
+        <div className="container-footer">            
+            <div className="logo-footer">
+                <a href="#"><img className="petdocsLogo" src= {ImagenLogo} alt="logoPetDocs" /></a>
+            </div>            
+            <div className="redes-footer">
+                <div className="rs-footer">
+                   <a href="https://www.instagram.com/"> <img src={ImagenInstagram} alt="Instagram" /></a>
+                </div>
+                <div className="rs-footer">
+                   <a href="https://web.whatsapp.com/"> <img src={ImagenWhatsapp} alt="Whatsapp" /></a>
+                </div>
+                <div className="rs-footer">
+               <a href="mailto:petdocs@example.com"><img src={ImagenMail} alt="Mail" /></a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
