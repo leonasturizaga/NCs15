436e7efce8013f662fa6b6f28fbb70c0c06dd3bd
Frere-Luz <algo_de_luz@hotmail.com>
2024-06-04 11:57:16 -0300

    La card de la mascota, falta responsive, y link a ppal corrijo index css y app css, comento las rutas para probar la vista

--- petdocs/src/App.jsx
+++ petdocs/src/App.jsx
@@ -1,62 +1,62 @@
 // //import { useState } from 'react'
+import React from 'react';
 // import Hero from './components/Hero'
 // import './App.css'
 
-// function App() {
-//   return (
-//     <>
-//       <div>
-//         <Hero />
-//       </div>
+function App() {
+  return (
+    <>
+      <div>
+        <PetCard />
+      </div>
       
-//     </>
-//   )
-// }
+    </>
+  )
+}
 
-// export default App
+export default App
 
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Header from './components/Header';
 
-import imagePath from "./assets/logo-with-name.jpeg";
+// import imagePath from "./assets/LogoPetDocs.png";
 // import "bootstrap/dist/css/bootstrap.css";
 // import "bootstrap/dist/js/bootstrap.min.js";
 
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Body from './components/Body';
-import Footer from './components/Footer';
-import Register from './components/Register';
-import Login from './components/Login';
+// import Navbar from './components/Navbar';
+// import Hero from './components/Hero';
+// import Body from './components/Body';
+// import Footer from './components/Footer';
+// import Register from './components/Register';
+// import Login from './components/Login';
+import PetCard from './pages/PetCard';
 import './App.css';
 
 
-let items = ["Inicio", "Nosotros"];
+// let items = ["Inicio", "Nosotros"];
 
-const App = () => (
+// const App = () => (
 
-  <Router>
-    <div className="App">
+//   <Router>
+//     <div className="App">
    
-      <Header brandName={"PetDocs"} imageSrcPath={imagePath} navItems={items} />
-      {/* <p>{repeatLoremIpsum(10)}</p> */}
+//       <Header brandName={"PetDocs"} imageSrcPath={imagePath} navItems={items} />     
+//       <Navbar />
+//       <Routes>
+//         <Route path="/" element={
+//           <>
+//             <Hero />
+//             <Body />
+//           </>
+//         } />
+//         <Route path="/register" element={<Register />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/petcard" element={<PetCard />} />
+//       </Routes>
+//       <Footer />
+//     </div>
+//   </Router>
+// );
 
-      <Navbar />
-      <Routes>
-        <Route path="/" element={
-          <>
-            <Hero />
-            <Body />
-          </>
-        } />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-
-      </Routes>
-      <Footer />
-    </div>
-  </Router>
-);
-
-export default App;
+// export default App;
