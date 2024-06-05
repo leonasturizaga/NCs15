0d7f40ca512d36b048c2da37a54293a586a91735
Diego Sanabria <sanabria.diego@outlook.com>
2024-06-05 17:41:47 -0400

    added endpoint for uploading a pet picture for a pet that has already been created

--- vetapi/petrecord/urls.py
+++ vetapi/petrecord/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import validate_owner
 from .views import create_owner
 from .views import update_owner  # se puede poner directamente import .views
-from .views import validate_owner, ImageUploadView, create_pet, EventListCreate, EventRetrieveUpdateDestroy
+from .views import validate_owner, ImageUploadView, create_pet, EventListCreate, EventRetrieveUpdateDestroy, PetImageUploadView
 
 urlpatterns = [
     path('validate_owner/', validate_owner, name='validate_owner'),
@@ -12,5 +12,6 @@ urlpatterns = [
     path('owner/<str:nick>/new_pet/', create_pet, name='create_pet'),
     path('events/', EventListCreate.as_view(), name='event-list-create'),
     path('events/<int:pk>/', EventRetrieveUpdateDestroy.as_view(), name='event-retrieve-update-destroy'),
+    path('pet_picture/<int:pet_id>/', PetImageUploadView.as_view(), name='pet_picture')
 
 ]
