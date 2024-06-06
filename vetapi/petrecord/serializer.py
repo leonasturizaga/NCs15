ca72fe760bfdafb70005d1344dd7e6ca4576a546
SergioGacal <gacalsergio@gmail.com>
2024-06-06 18:45:54 -0300

    Se sube corrección de incidencia que no permitía el alta de eventos

--- vetapi/petrecord/serializer.py
+++ vetapi/petrecord/serializer.py
@@ -41,7 +41,7 @@ class EventSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Event
-        fields = '__all__'        before 6/6
+        fields = '__all__'
         #fields = ['id', 'pet_id', 'pet_name', 'title', 'description', 'startdate', 'enddate', 'url', 'duracion', 'falta']
 
     def get_duracion(self, obj):
