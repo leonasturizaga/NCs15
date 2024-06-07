e6bf9138ff1811c185a6ec4b26d8d0b03e9d0762
leonAsturizaga <leonasturizaga@gmail.com>
2024-06-06 22:40:46 -0300

    login ok

--- petdocs/src/pages/Home.jsx
+++ petdocs/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 // src/pages/Home.jsx
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import Caracteristicas from '../components/Caracteristicas';
@@ -13,9 +14,15 @@ import Footer from '../components/Footer';
 
 
 const Home = () => {
+  const location = useLocation();
+  const { nick } = location.state || {};  
   return (
     <div>
+      <div>
       <h3 className="title-h1">PetDocs home</h3>
+      <p className='text-banner'>Bienvenido {nick}!</p>
+      </div>
+
       <Caracteristicas />
       <PetCard />
     </div>
