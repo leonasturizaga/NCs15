a9ecb72014fe09698f74b01cb0b6efbe0c0a75df
Dario A. Leal <l.dario.a@gmail.com>
2024-06-10 21:53:31 -0300

    cambios en header/usuario logueado
    El header ahora muestra "Login" si no hay usuario logueado, o muestra el nombre del usuario con la opción de que haga "Logout".
    Tb borré archivo 'notas-daro', no tenía utilidad ya.

--- petdocs/src/components/Header.jsx
+++ petdocs/src/components/Header.jsx
@@ -2,11 +2,11 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../context/UserContext';
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import "./css/Header.css";
 
 const Header = ({ brandName, pathLogo, navItems }) => {
-  const { nick } = useContext(UserContext);
+  const { nick, logout } = useContext(UserContext);
 
   const [selectedIndex, setSelectedIndex] = useState(-1);
   const [isCollapsed, setIsCollapsed] = useState(true);
@@ -42,9 +42,16 @@ const Header = ({ brandName, pathLogo, navItems }) => {
   }, []);
 
   return (
-    <nav className={`navbar fixed-top navbar-expand-md navbar-light shadow ${scrolled ? 'navbar-custom-bg-color' : 'bg-white'}`}>
+    <nav
+      className={`navbar fixed-top navbar-expand-md navbar-light shadow ${
+        scrolled ? "navbar-custom-bg-color" : "bg-white"
+      }`}
+    >
       <div className="container-fluid">
-        <a className="navbar-brand" onClick={() => handleNavItemClicked(0, '/')}>
+        <a
+          className="navbar-brand"
+          onClick={() => handleNavItemClicked(0, "/")}
+        >
           <img
             src={pathLogo}
             width="90"
@@ -54,30 +61,96 @@ const Header = ({ brandName, pathLogo, navItems }) => {
           />
         </a>
         <button
-          className={`navbar-toggler ${scrolled ? 'navbar-custom-toggler' : ''}`}
+          className={`navbar-toggler ${
+            scrolled ? "navbar-custom-toggler" : ""
+          }`}
           type="button"
           onClick={handleToggleCollapse}
           aria-label="Toggle navigation"
         >
-          <span className={`navbar-toggler-icon ${scrolled ? 'navbar-custom-toggler-icon' : ''}`}></span>
+          <span
+            className={`navbar-toggler-icon ${
+              scrolled ? "navbar-custom-toggler-icon" : ""
+            }`}
+          ></span>
         </button>
 
-        <div className={`collapse justify-content-end navbar-collapse ${isCollapsed ? '' : 'show'}`}>
+        <div
+          className={`collapse justify-content-end navbar-collapse ${
+            isCollapsed ? "" : "show"
+          }`}
+          id="navbarNavDropdown"
+        >
           <ul className="navbar-nav align-items-center mr-auto mb-2 mb-md-1">
-            <li>
-            <p className='text-banner'>Nick: {nick}</p>
-            </li>
             {navItems.map((item, index) => (
               <li
                 key={item}
                 className="nav-item"
-                onClick={() => handleNavItemClicked(index, index === 0 ? '/' : '/nosotros')}
+                onClick={() =>
+                  handleNavItemClicked(index, index === 0 ? "/" : "/nosotros")
+                }
               >
-                <a className={`nav-link ${selectedIndex === index ? 'active fw-bold' : ''} ${scrolled ? 'navbar-custom-text-color' : ''}`} href="#">
+                <a
+                  className={`nav-link ${
+                    selectedIndex === index ? "active fw-bold" : ""
+                  } ${scrolled ? "navbar-custom-text-color" : ""}`}
+                  href="#/"
+                >
                   {item}
                 </a>
               </li>
             ))}
+
+            {/* Último ítem del navbar: login o el nombre del usuario. */}
+
+            <li
+              className="nav-item dropdown"
+              onClick={() => setIsCollapsed(true)}
+            >
+              {/* Si el nick no es vacío (usuario logueado), se muestra el nick y un botón para logout, sino se muestra Login. */}
+              {nick !== "" ? (
+                <>
+                  <a
+                    className={`nav-link dropdown-toggle ${
+                      scrolled ? "navbar-custom-text-color" : ""
+                    }`}
+                    href="#/"
+                    id="navbarDropdownMenuLink"
+                    data-bs-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    {nick}
+                  </a>
+                  <div
+                    // className="dropdown-menu"
+                    className={`dropdown-menu ${scrolled ? "navbar-custom-bg-color" : "bg-white"}`}
+                    aria-labelledby="navbarDropdownMenuLink"
+                  >
+                    <a
+                      // className="dropdown-item" 
+                      className={`dropdown-item no-hover-bg ${scrolled ? "navbar-custom-text-color" : ""}`}
+                      href="#/"
+                      onClick={() => {
+                        logout();
+                        navigate("/");
+                      }}
+                    >
+                      Logout
+                    </a>
+                  </div>
+                </>
+              ) : (
+                <Link
+                  to="/login"
+                  className={`nav-link ${
+                    scrolled ? "navbar-custom-text-color" : ""
+                  }`}
+                >
+                  Login
+                </Link>
+              )}
+            </li>
           </ul>
         </div>
       </div>
